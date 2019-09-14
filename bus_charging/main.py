dab32cb957f8c471f6d8c689a9c889c89ca27371
Terrence Wong <te_ong@encs.concordia.ca>
2019-09-14 00:14:46 -0400

    still not working yet

--- bus_charging/main.py
+++ bus_charging/main.py
@@ -33,7 +33,7 @@ routes = ioIn.getRoute()
 NUM_ROUTES = len(routes)
 LONGEST_ROUTE = 0
 
-for r in routes[0]:
+for r in routes:
     if len(r) > LONGEST_ROUTE:
         LONGEST_ROUTE = len(r)
 
@@ -43,7 +43,6 @@ struct route_stop_s {
     unsigned int station_id;
     float distance_m;
 };
-
 """
 route_stop_dtype = np.dtype([("station_id", np.uint32), ("distance_m", np.float32)])
 register_dtype(route_stop_dtype, "route_stop_s")
@@ -55,9 +54,10 @@ routes_np = np.empty((NUM_ROUTES, LONGEST_ROUTE), dtype=route_stop_dtype)
 routes_lengths_np = np.empty((NUM_ROUTES), dtype=np.uint32)
 
 global_stop_id = 0
-for r_index, r in enumerate(routes[0]):
-    routes_lengths_np[r_index] = len(r["stop_list"])
-    for s_index, s in r["stop_list"]:
+for r_index, r in enumerate(routes):
+    routes_lengths_np[r_index] = len(r)
+    print(" r is " + str(r))
+    for s_index, s in enumerate(r):
         gps = (s["lat"], s["lon"])
         if gps not in stops:
             stops[gps] = {}
@@ -65,9 +65,30 @@ for r_index, r in enumerate(routes[0]):
             stops[gps]["global_stop_id"] = global_stop_id
             global_stop_id += 1
         stops[gps]["routes"].append((r_index, s_index))
+
         routes_np[r_index][s_index]["station_id"] =  stops[gps]["global_stop_id"]
         routes_np[r_index][s_index]["distance_m"] =  s["distance_m"]
 
-print("XXXX " + str(stops))
-print("YYYY " + str(routes_np))
-        
+
+# the stop with the most number of routes crossing it
+LONGEST_STOPS = 0
+for s in stops:
+    if len(s["routes"]) > LONGEST_STOPS:
+        LONGEST_STOPS = len(s["routes"])
+
+# this uint32 is packed with the route id in the upper 16 bits and the index into the route in the lower 16
+stops_np = np.empty((global_stop_id, LONGEST_STOPS), dtype=np.uint32)
+stops_lengths_np = np.empty((global_stop_id), dtype = np.uint32)
+
+for s in stops:
+    stops_lengths_np[s["station_id"]] = len(s["routes"])
+    for index, r in enumerate(s["routes"]):
+        (route_id, stop_index) = r
+        stops_np[s["station_id"]][index] = route_id << 16 + stop_index
+
+#routes_lengths_np and stops_lengths_np get sent into constant cache
+# routes_np and stops_np get set as regular global memry
+
+# what's left is to make a list of stops that have a charging station. This is probably a bit array we'll copy into shared memory
+# I guess we'll initialize each run with a random arrangement of stations for however many we're installing at this round
+
